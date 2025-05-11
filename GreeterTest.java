bbd915ad14dab3c310a8f88cf8f77db49431f14a
Hakim NZR <anazary21@posta.pau.edu.tr>
2025-05-12 01:22:35 +0300

    Update GreeterTest.java

--- GreeterTest.java
+++ GreeterTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 // test the code
 public class GreeterTest {
- 
+  
     @Test
     public void testThatGreeterReturnsTheCorrectGreeting() {
         assertThat(new Greeter().getGreeting()).isEqualTo("Hello, World!");
